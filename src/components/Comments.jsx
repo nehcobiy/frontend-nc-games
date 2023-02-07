e268adc7db1b96cc947a0acf11071cc8ca066e88
nehcobiy <nehcobiy@LAPTOP-TDTL4JV3.localdomain>
2023-02-07 15:35:01 +0000

    finished task6 comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,7 +4,7 @@ import { matchUserImgs } from "../utils/user";
 import { fetchUsers } from "../utils/api";
 
 export default function Comments({ review_id }) {
-  const [comments, setComments] = useState([{}]);
+  const [comments, setComments] = useState([]);
   console.log(comments);
 
   useEffect(() => {
@@ -18,43 +18,50 @@ export default function Comments({ review_id }) {
       }
     );
   }, []);
+  if (comments.msg === "no comments exist for this id") {
+    return (
+      <section>
+        <hr className="comment-line" />
+        <h1 className="comment-title">0 Comments</h1>
+      </section>
+    );
+  } else
+    return (
+      <section>
+        <hr className="comment-line" />
+        <h1 className="comment-title">{comments.length} Comments</h1>
+        <ul className="comment-list">
+          {comments.map((comment) => {
+            return (
+              <li key={comment.comment_id} className="single-comment-container">
+                <section className="single-comment">
+                  <img
+                    src={comment.avatar}
+                    alt={`${comment.username}'s profile`}
+                    key={comment.comment_id}
+                    className="user-avatar"
+                  />
 
-  return (
-    <section>
-      <hr className="comment-line" />
-      <h1 className="comment-title">{comments.length} Comments</h1>
-      <ul className="comment-list">
-        {comments.map((comment) => {
-          return (
-            <li key={comment.comment_id} className="single-comment-container">
-              <section className="single-comment">
-                <img
-                  src={comment.avatar}
-                  alt={`${comment.username}'s profile`}
-                  key={comment.comment_id}
-                  className="user-avatar"
-                />
-
-                <section className="single-comment-text">
-                  <h3 className="single-comment-header-container">
-                    <p className="single-comment-header">
-                      {comment.author}&nbsp;
-                    </p>
-                    <p
-                      className="single-comment-header"
-                      id="single-comment-date"
-                    >
-                      {`${comment.created_at.slice(0, 10)} 
+                  <section className="single-comment-text">
+                    <h3 className="single-comment-header-container">
+                      <p className="single-comment-header">
+                        {comment.author}&nbsp;
+                      </p>
+                      <p
+                        className="single-comment-header"
+                        id="single-comment-date"
+                      >
+                        {`${comment.created_at.slice(0, 10)} 
                       ${comment.created_at.slice(11, 16)}`}
-                    </p>
-                  </h3>
-                  <p className="single-comment-body">{comment.body}</p>
+                      </p>
+                    </h3>
+                    <p className="single-comment-body">{comment.body}</p>
+                  </section>
                 </section>
-              </section>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
 }
